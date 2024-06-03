fbdcb01eb2911398f56b06f9ea9d85678e80dae2
magickoo <magickhu@gmail.com>
2024-06-03 12:19:17 +0530

    feat: Create Hall

--- halls/views.py
+++ halls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .models import Hall
 # Create your views here.
 def home(request):
     return render(request,'halls/home.html')
@@ -11,3 +12,18 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
     
+#def create_view(request):
+    #if request.method == 'POST':
+        # get the form data
+        # validate the form data
+        # create hall
+        # save hall
+    #else:
+        # Create a form for hall
+        # return the hall
+        
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
