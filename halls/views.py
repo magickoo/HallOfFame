13d7a023f7cec296e56def600147921535286991
magickoo <magickhu@gmail.com>
2024-06-06 19:53:26 +0530

    feat: Youtube api added

--- halls/views.py
+++ halls/views.py
@@ -6,7 +6,13 @@ from django.contrib.auth import login,logout , authenticate
 from .models import Hall, Video
 from .forms import VideoForm, SearchForm
 from django.forms import formset_factory
+from django.http import Http404
+from django.forms.utils import ErrorList
+import urllib
+import requests
 
+
+YOUTUBE_API_KEY = 'AIzaSyBIKzmId4r-ClE4t-lpqts8n0lUxeAWWJU'
 def home(request):
     return render(request,'halls/home.html')
 def dashboard(request):
@@ -21,16 +27,29 @@ def add_video(request,pk):
     
     form = VideoForm()
     search_form = SearchForm()
+    hall =  Hall.objects.get(pk = pk)
+    if not hall.user == request.user:
+        raise Http404
     if request.method == 'POST':
         #create
         filled_form = VideoForm(request.POST)
         if filled_form.is_valid():
             video = Video()
+            video.hall = hall
             video.url = filled_form.cleaned_data['url']
-            video.title = filled_form.cleaned_data['title']
-            video.youtube_id = filled_form.cleaned_data['youtube_id']
-            video.hall = Hall.objects.get(pk=pk)
-            video.save() #will put it into database
+            parsed_url =urllib.parse.urlparse(video.url)
+            video_id = urllib.parse.parse_qs(parsed_url.query).get('v')
+            if video_id: 
+                video.youtube_id = video_id[0]
+                response = requests.get(f'https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&id={ video_id[0] }&key=[YOUTUBE_API_KEY]')
+                json = response.json()
+                title = json['items'][0]['snippet']['title']
+                print(title)
+                video.title = title
+                video.save()
+                
+                #video.title = 
+                #video.save() #will put it into database
     
     return render(request,'halls/add_video.html',{'form':form , 'search_form': search_form})
 
